272058c75dc1190b97108ba57994efdcb13ef26c
EC2 Default User <ec2-user@ip-172-31-84-125.ec2.internal>
2022-11-01 09:28:21 +0000

    initial commit

---
+++ run_kubernetes.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath=binhnguyen058/prediction-app:latest
+echo "Docker ID and Image: $dockerpath"
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run prediction-app --image=$dockerpath --port=8000 --labels app=binhnguyen058
+
+
+# Step 3:
+# List kubernetes pods
+sleep 100
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward prediction-app 8000:8000
